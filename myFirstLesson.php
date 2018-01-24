c22412086a8e30a97c2c248b4795a02b42b3bfd8
aliekov.u <aliekov@phystech.edu>
2018-01-24 14:40:13 +0300

    myFirstClass

---
+++ myFirstLesson.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * Created by PhpStorm.
+ * User: aliekov.u
+ * Date: 24.01.2018
+ * Time: 14:32
+ */
+
+class Coor
+{
+    var $name;
+    var $addr;
+    function Name(){
+        echo "sdadas";
+    }
+}
+
+$object = new Coor;
+$object->name="ALex";
+echo $object->name;
